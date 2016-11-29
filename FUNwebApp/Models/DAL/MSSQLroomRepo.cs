a783a0f087d687bd92e0b357ca97a56149eb365f
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2016-11-29 15:01:33 +0100

    Updated the MSSQLroomRepo

--- FUNwebApp/Models/DAL/MSSQLroomRepo.cs
+++ FUNwebApp/Models/DAL/MSSQLroomRepo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Web;
@@ -8,19 +10,75 @@ namespace FUNwebApp.Models.DAL
 {
     public class MSSQLroomRepo : IRoomRepo
     {
+        private readonly string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Teun\Source\Repos\FUN2killerapp\KillerAppFUN2\KillerAppFUN2\RPGdata.mdf;Integrated Security=True";
+
         public string getRoomLayout(int roomID)
         {
-            throw new NotImplementedException();
+            string layout = "";
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT RoomLayOut FROM Rooms WHERE RoomID = @roomID", connection))
+                {
+                    cmd.Parameters.Add("@roomID", SqlDbType.Int).Value = roomID;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            layout = reader.GetString(0);
+                        }
+                    }
+                }
+            }
+            return layout;
         }
 
         public Point GetPlayerSpawnPoint(int roomID)
         {
-            throw new NotImplementedException();
+            Point spawnPoint = new Point();
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT PlayerSpawnX, PlayerSpawnY FROM Rooms WHERE RoomID = @roomID", connection))
+                {
+                    cmd.Parameters.Add("@roomID", SqlDbType.Int).Value = roomID;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int x = reader.GetInt32(0);
+                            int y = reader.GetInt32(1);
+                            spawnPoint = new Point(x, y);
+                        }
+                    }
+                }
+            }
+            return spawnPoint;
         }
 
-        public List<Point> GetEnemySpawnPoints(int roomID)
+        public List<Point> GetEnemySpawnPoints(int roomID) //WIP
         {
-            throw new NotImplementedException();
+            List<Point> enemySpawnPoints = new List<Point>();
+            /*
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT SpawnX, SpawnY FROM EnemyRooms WHERE RoomID = @roomID", connection))
+                {
+                    cmd.Parameters.Add("@roomID", SqlDbType.Int).Value = roomID;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int x = reader.GetInt32(0);
+                            int y = reader.GetInt32(1);
+                            spawnPoint = new Point(x, y);
+                        }
+                    }
+                }
+            }
+            */
+            return enemySpawnPoints;
         }
 
         public List<Point> GetObjectSpawnPoints(int roomID)
